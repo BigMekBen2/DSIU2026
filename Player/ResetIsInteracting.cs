420bb4dbb73a4cc2ae1d2798c1ad90bae872137f
Benjamin J Thompson <bigmekben@gmail.com>
2026-03-10 11:01:19 -0400

    Episode 004 Complete

---
+++ Player/ResetIsInteracting.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SG
+{
+    public class ResetIsInteracting : StateMachineBehaviour
+    {
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            animator.SetBool("isInteracting", false);
+        }
+    }
+}
